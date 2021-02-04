5a37369020ca9effc96811546dc79d903462468d
paulcurtis <paulcurtis62@googlemail.com>
2021-02-04 18:25:00 +0000

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AMS stock is going UP and DOWN like a yoyo hopefully it wiil GO to the moon'));
+app.get('/', (req, res) => res.send('AMS stock is going UP and DOWN like a yoyo hopefully it wiil GO to the moon and beyond and then a little bit further and then a few more inches'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
